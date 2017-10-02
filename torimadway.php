456627e2925abf6ad9b6c7dedb6bc85a7b9a6030
tmadway <31747658+tmadway@users.noreply.github.com>
2017-10-02 17:53:50 -0400

    install materialize

--- torimadway.php
+++ torimadway.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 	<head>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+		
  		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>Homepage</title>
@@ -119,6 +122,9 @@
   		<script src="starter-template/js/materialize.js"></script>
   		<script src="starter-template/js/init.js"></script>
   		<script>$(".button-collapse").sideNav();</script>
+		
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      		<script type="text/javascript" src="js/materialize.min.js"></script>
 
 	</body>
 </html>
