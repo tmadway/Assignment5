0fc9469d52e8b37adde93892f94671c48115030d
tmadway <31747658+tmadway@users.noreply.github.com>
2017-10-02 18:13:00 -0400

    add overrides.css

--- hillarychun.php
+++ hillarychun.php
@@ -2,8 +2,10 @@
 <html lang="en">
 
 	<head>
+		<link type="text/css" rel="stylesheet" href="css/overrides.css"  media="screen,projection"/>
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
 		
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
