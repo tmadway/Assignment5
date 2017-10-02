6a313991ede5f53b568f07494221a60a78c284be
rosecricchio <rcricchi@u.rochester.edu>
2017-10-02 18:35:15 -0400

    fixed titles

--- rosecricchio.php
+++ rosecricchio.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<title>Hillary Chun</title>
+		<title>Rose Cricchio</title>
 
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  		<link href="starter-template/css/materialize.css" type="text/css" rel="stylesheet" media="screen"/>
