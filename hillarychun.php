ed79d48c749f5c252644933e2f3259a712f20eb5
rosecricchio <rcricchi@u.rochester.edu>
2017-10-04 15:39:38 -0400

    css links

--- hillarychun.php
+++ hillarychun.php
@@ -2,18 +2,13 @@
 <html lang="en">
 
 	<head>
-		<link type="text/css" rel="stylesheet" href="css/overrides.css"  media="screen,projection"/>
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-		
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>Hillary Chun</title>
 
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
- 		<link href="starter-template/css/materialize.css" type="text/css" rel="stylesheet" media="screen"/>
-  		<link href="starter-template/css/style.css" type="text/css" rel="stylesheet" media="screen"/>
+ 		<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen"/>
+  		<link href="css/override.css" type="text/css" rel="stylesheet" media="screen"/>
 	</head>
 	
 	<body>
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 	<head>
-		
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>Homepage</title>
@@ -10,7 +9,6 @@
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  		<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen"/>
   		<link href="css/override.css" type="text/css" rel="stylesheet" media="screen"/>
-
 	</head>
 
 	<body>
